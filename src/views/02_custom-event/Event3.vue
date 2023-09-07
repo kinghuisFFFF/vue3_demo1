429e5d6459ea86a66dfab0ccdbc65d5340870ed9
c.wang <c.wang@hang-shu.com>
2023-09-07 16:00:59 +0800

    提交没有格式化的代码

---
+++ src/views/02_custom-event/Event3.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="son2">
+        <p>我是子组件3</p>
+
+
+
+    </div>
+</template>
+<script setup lang="ts">
+import { ref, reactive } from "vue"
+
+
+
+
+</script>
+<style lang="scss" scoped>
+.son2 {
+    width: 400px;
+    height: 200px;
+    background: pink;
+}
+</style>
---
+++ src/views/10_mock/MockData.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+
+<p>工具类</p>
+
+
+  </div>
+</template>
+<script setup lang="ts">
+import {ref,reactive} from "vue"
+
+
+
+
+</script>
+<style lang="scss" scoped>
+.main{
+    width: 400px;
+    height: 200px;
+    background: pink;
+}
+
+
+
+</style>
